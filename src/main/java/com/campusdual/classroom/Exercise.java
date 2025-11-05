8e472faa882457573966e1c0a588849458f93ab9
Diego Armando Castillo Montoya <castillodiego2401@gmail.com>
2025-11-05 08:08:23 +0100

    Solved Exercise 27

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,6 +2,7 @@ package com.campusdual.classroom;
 
 public class Exercise {
     public static void main(String[] args) {
+        throw new RuntimeException("¡Algo salió mal en el main del ejercicio!");
 
     }
 }
